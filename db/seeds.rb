282edbbbde4ffe667f6313832cd8506a4eedfabb
Julia Schwenner <julia.schwenner@gmail.com>
2020-05-21 18:45:16 +0200

    Mister Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ingredients = [{name: 'lemon'}, {name: 'ice'}, {name: 'mint leaves'}, {name: 'vodka'}, {name: 'rum'}]
+
+Ingredient.create!(ingredients)
